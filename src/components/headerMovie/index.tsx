49519ad454335e64398276d7ac06718c3555960b
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-06-15 20:48:36 +0100

    Showing favourite icon on Movie Details page

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -6,6 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import { MovieDetailsProps } from "../../types/interfaces";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const styles = {
   root: {
@@ -15,15 +17,25 @@ const styles = {
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
+  const favourites = JSON.parse(localStorage.getItem("favourites"));
+  const isFavourite = favourites.some((fav) => fav.id === movie.id);
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
+      {isFavourite && (
+        <Avatar sx={styles.avatar}>
+          <FavoriteIcon />
+        </Avatar>
+      )}
       <Typography variant="h4" component="h3">
         {movie.title}
         {"   "}
